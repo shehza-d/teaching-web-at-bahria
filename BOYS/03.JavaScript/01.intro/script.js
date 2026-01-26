9af029a1fc6f8f2ea58d982501570887879a0bab
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-01-26 18:35:16 +0500

    CLASS 2 JS

--- BOYS/03.JavaScript/01.intro/script.js
+++ BOYS/03.JavaScript/01.intro/script.js
@@ -1,9 +1,38 @@
 let a = "Shehzad";
 
+a = "karachi"; // good
+
 console.log(a);
+
+const email = "shehzad@gmail.com";
+
+email = "testing"; // error
+
+console.log("email:", email);
+
 console.log(8 + 3);
 
-// let a = 3;
+let a = 3;
 let b = 6;
 
 console.log(a + b);
+
+let a = prompt("what is your name?");
+
+console.log("welcome to ", a);
+
+let num1 = Number(prompt("enter first number"));
+let num2 = Number(prompt("enter second number"));
+
+console.log(typeof num1);
+
+console.log(num1 + num2);
+
+let c = 5;
+let C = 6;
+
+console.log(c);
+
+// camel case   https://miro.medium.com/v2/1*sPnKG88r91LirwmrOLWVEA.png
+let thisIsMyNewVariable = 1;
+let userName = "shehzad";
---
+++ BOYS/03.JavaScript/02.data-types/app.js
@@ -0,0 +1,9 @@
+let a = ""; // string (text)
+let b = 5; // number
+let c = false; // boolean
+let d;
+let e = Null;
+// symbol
+let n = BigInt(4343943439434934343434);
+
+console.log(d);
