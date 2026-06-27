9674e394d3deae5098688507dc8f59884095738c
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-06-27 13:29:23 +0500

    CLASS: Nested for loops

---
+++ BOYS/04.JavaScript/06.loop/nested-loops.js
@@ -0,0 +1,39 @@
+// let i = 0;
+
+// while (i < 11) {
+//     console.log(i);
+//     i++
+// }
+
+// const arr = ["ahmed", "huzaifa", "faizan", "talha"];
+
+// while (arr.length) {
+//   console.log(arr.pop());// 0
+// }
+
+// for (let i = 1; i <= 12; i++) {
+//   for (let j = 1; j <= 2; j++){
+//     console.log("hello");
+//   }
+// }
+
+// for (let i = 1; i <= 4; i++) {
+//   console.log("Row ", i);
+
+//   for (let j = 1; j <= 6; j++) {
+//     console.log("    Student ", j);
+//   }
+// }
+
+const a = [
+  [4, 6, 7],
+  [9, 100, 0],
+  [1, 7, 2],
+];
+console.log(a[2][0]);
+
+const matrice = [
+  [4, 6, 7],
+  [9, 100, 0],
+  [1, 7, 2],
+];
---
+++ CodeWars/01.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59dd3ccdded72fc78b000b25
+
+const arr = ["apple", "banana", "mango", "watermelon", "grape"];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
